12bab59dd483981cec90a6170849446c6578eca0
@jmarcos-cano <jmarcos.cano@gmail.com>
2020-07-18 01:55:55 -0600

    @jmarcos-cano - Adds new structure

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # katacoda-scenarios
-![1](docs/bootcamp.png)
 
-<!-- 
-<iframe src="https://slides.com/marcoscano/deck-76e37f/embed" width="576" height="420" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> -->
+Kontinu ofrece una serie de Bootcamps, este es parte del material de apoyo para los bootcamps de Docker, Kubernetes y Devops
---
+++ help.sh
@@ -0,0 +1,14 @@
+katacoda courses
+katacoda scenarios
+katacoda scenarios:create --help
+katacoda scenarios:create 
+npm install -g katacoda-cli
+git clone git@github.com:kontinu/katacoda-scenarios.git
+cd katacoda-scenarios/
+katacoda create:course
+katacoda courses:create
+katacoda courses:add:scenarios
+katacoda --help
+katacoda scenarios
+katacoda courses
+katacoda courses:delete
